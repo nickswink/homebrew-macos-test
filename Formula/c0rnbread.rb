13cdf26dba1248a5cf7687fa558a6ac364341f86
c0rnbread <57839593+nickswink@users.noreply.github.com>
2026-01-09 10:53:10 -0500

    Update c0rnbread.rb

--- Formula/c0rnbread.rb
+++ Formula/c0rnbread.rb
@@ -2,7 +2,7 @@ class C0rnbread < Formula
     desc "Test"
     homepage "https://github.com/nickswink/"
     url "https://github.com/nickswink/homebrew-macos-test/releases/download/release/c0rnbread-1.0.0.tar.gz"
-    sha256 "82a786008b1aa76ce2c2874dd2b9c70f4167262748c65bc1dc36546afbb10e95"
+    sha256 "91a2a85ce5d0e7511daa4bfc22e87df2fe1bf67ff3d7fc9769d90b1193e95eea"
     version "1.0.0"
 
     def install
@@ -18,7 +18,7 @@ class C0rnbread < Formula
 
     def caveats
         <<~EOS
-        To finish installation, add the following line to .zshrc or .bashrc  
+        To finish installation, add the following line to .zshrc or .bashrc
   source #{bin}/completion.sh
 Then reopen your terminal app
         EOS
